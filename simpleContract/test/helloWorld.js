042bfc18a5cbbe93557e8c34a25fd77618a513e1
devcraft1 <ukonudennis64@gmail.com>
2021-09-27 10:39:53 +0100

    front-end

--- simpleContract/test/helloWorld.js
+++
@@ -1,9 +0,0 @@
-const HelloWorldToString = artifacts.require('HelloWorld');
-
-contract('HelloWorld', () => {
-  it('should return a string', () => {
-    const helloWorld = HelloWorldToString.deployed();
-    const hello = helloWorld.hello();
-    assert(hello === 'Hello world');
-  });
-});
--- simpleContract/test/simpleContract.js
+++
@@ -1,9 +0,0 @@
-const SimpleContract = artifacts.require('SimpleContract');
-
-contract('SimpleContract', () => {
-  it('should deploy a smart contract', async () => {
-    const simpleContract = await SimpleContract.deployed();
-    // console.log(simpleContract.address);
-    assert(simpleContract.address !== '');
-  });
-});
