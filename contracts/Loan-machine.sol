882b9ed3f90258b21259f3e07a6a3ba1ce9bfc36
devcraft1 <ukonudennis64@gmail.com>
2021-10-24 05:32:54 +0100

    state-machine

---
+++ contracts/Loan-machine.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.0;
+
+contract StateMachine {
+    enum State {
+        PENDING,
+        ACTIVE,
+        CLOSED
+    }
+    State public state = State.PENDING;
+    uint256 public amount;
+    uint256 public interest;
+    uint256 public end;
+    uint256 public duration;
+    address payable public borrower;
+    address payable public lender;
+
+    constructor(
+        uint256 _amount,
+        uint256 _interest,
+        uint256 _duration,
+        address payable _borrower,
+        address payable _lender
+    ) {
+        amount = _amount;
+        interest = _interest;
+        duration = _duration;
+        borrower = _borrower;
+        lender = _lender;
+    }
+
+    function fund() external payable {
+        require(msg.sender == lender, "only lender can lend");
+        require(
+            address(this).balance == amount,
+            "can only lend the exact amount"
+        );
+        _transitionTo(State.ACTIVE);
+        borrower.transfer(amount);
+    }
+
+    function reimburse() external payable {
+        require(msg.sender == borrower, "only borrower can reimburse");
+        require(
+            msg.value == amount + interest,
+            "borrower need to reimburse exactly amount + interest"
+        );
+        _transitionTo(State.CLOSED);
+        lender.transfer(amount + interest);
+    }
+
+    function _transitionTo(State to) internal {
+        require(to != State.PENDING, "cannot go back to pending");
+        require(to != state, "cannot transition to same state");
+        if (to == State.ACTIVE) {
+            require(
+                state == State.PENDING,
+                "cannot only go to active from pending"
+            );
+            state = State.ACTIVE;
+            end = block.timestamp + duration;
+        }
+        if (to == State.CLOSED) {
+            require(
+                state == State.ACTIVE,
+                "cannot only go to closed from active"
+            );
+            require(block.timestamp >= end, "loan hasnt matured yet");
+            state = State.CLOSED;
+        }
+    }
+}
--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,5 +1,74 @@
 // SPDX-License-Identifier:MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-contract Vote {}
+contract Voting {
+    mapping(address => bool) public voters;
+    struct Choice {
+        uint256 id;
+        string name;
+        uint256 votes;
+    }
+    struct Ballot {
+        uint256 id;
+        string name;
+        Choice[] choices;
+        uint256 end;
+    }
+    mapping(uint256 => Ballot) ballots;
+    uint256 nextBallotId;
+    address public admin;
+    mapping(address => mapping(uint256 => bool)) votes;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function addVoters(address[] calldata _voters) external onlyAdmin {
+        for (uint256 i = 0; i < _voters.length; i++) {
+            voters[_voters[i]] = true;
+        }
+    }
+
+    function createBallot(
+        string memory name,
+        string[] memory _choices,
+        uint256 offset
+    ) public onlyAdmin {
+        ballots[nextBallotId].id = nextBallotId;
+        ballots[nextBallotId].name = name;
+        ballots[nextBallotId].end = now + offset;
+        for (uint256 i = 0; i < _choices.length; i++) {
+            ballots[nextBallotId].choices.push(Choice(i, _choices[i], 0));
+        }
+        nextBallotId++;
+    }
+
+    function vote(uint256 ballotId, uint256 choiceId) external {
+        require(voters[msg.sender] == true, "only voters can vote");
+        require(
+            votes[msg.sender][ballotId] == false,
+            "voter can only vote once for a ballot"
+        );
+        require(
+            now < ballots[ballotId].end,
+            "can only vote until ballot end date"
+        );
+        votes[msg.sender][ballotId] = true;
+        ballots[ballotId].choices[choiceId].votes++;
+    }
+
+    //If `pragma experimental ABIEncoderV2`
+    function results(uint256 ballotId) external view returns (Choice[] memory) {
+        require(
+            now >= ballots[ballotId].end,
+            "cannot see the ballot result before ballot end"
+        );
+        return ballots[ballotId].choices;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+}
