f10f2e62a684df9e28b16ed3e027108d79ed5a27
Dennis Ukonu <ukonudennis64@gmail.com>
2022-11-23 10:52:12 +0100

    --add hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  // solidity: "0.8.17",
+  solidity: "0.8.0",
+};
+
+export default config;
